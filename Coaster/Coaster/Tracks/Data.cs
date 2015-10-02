adc95ac08f3c18d33d13008bd62aee60b0c9a7a4
Sean <sean>
2015-10-01 23:30:47 -0700

    Move Marshal methods into applicable classes
    TODO: Put SegmentTable somewhere proper

--- Coaster/Coaster/Tracks/Data.cs
+++ Coaster/Coaster/Tracks/Data.cs
@@ -10,5 +10,43 @@ namespace Coaster.Tracks // https://github.com/kevinburke/rct/blob/master/tracks
         public TrackElement[] Elements;
         public int Clearance;
         public ClearanceDirection ClearanceDirection;
+
+        public static Data Unmarshal(byte[] buf)
+        {
+            Data d = new Data();
+
+            List<TrackElement> elements = new List<TrackElement>();
+            for (int i = 0; i < buf.Length; i += 2)
+            {
+                TrackElement elem = TrackElement.Unmarshal((byte[])buf.GetValue(i, i + 1));
+                if (elem.Segment.Type == SegmentType.ELEM_END_OF_RIDE)
+                {
+                    break;
+                }
+
+                elements.Add(elem);
+            }
+
+            // XXX where is this data actually stored?
+            d.Clearance = 2;
+            d.ClearanceDirection = ClearanceDirection.CLEARANCE_ABOVE;
+
+            return d;
+        }
+
+        public static byte[] Marshal(Data d)
+        {
+            byte[] buf = new byte[(2 * d.Elements.Length) + 1];
+
+            for (int i = 0; i < d.Elements.Length; i++)
+            {
+                byte[] bts = TrackElement.Marshal(d.Elements[i]);
+                bts.CopyTo(buf, i * 2);
+            }
+
+            buf[buf.Length - 1] = 0xff; // track end
+
+            return buf;
+        }
     }
 }
